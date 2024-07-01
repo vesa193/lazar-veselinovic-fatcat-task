402c0784f8ef7a81cb3e2254b78b5487c0aabe2e
veselinovic93 <veselinovic93@hotmail.com>
2024-07-01 14:06:48 +0200

    chore(ts env): setup typescript environment

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import path from 'path';
+
+export default defineConfig({
+    plugins: [react()],
+    define: {
+        __CWD__: JSON.stringify(process.cwd()),
+    },
+    resolve: {
+        alias: {
+            '@homework-task': path.resolve(__dirname, './src'),
+            '@homework-task-components': path.resolve(
+                __dirname,
+                './src/components'
+            ),
+        },
+    },
+});
